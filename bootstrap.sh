e7a1b13ffe96c351c4bebcf54ff5d24b7761cf75
John Starich <johnstarich@johnstarich.com>
2018-05-07 23:31:20 -0500

    Add doc for using shell options, fix sourcing from same directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,44 @@
 #!/usr/bin/env bash
 # Bootstrap the library for use of standard functionality.
 
-# Exit if any command returns a non-zero code.
-set -e
+# Prevent shellcheck rules from being global
+true
 
 # Temporarily disable some checks to fail gracefully when invoking the wrong shell.
 # shellcheck disable=SC2015,SC2128
 [ -n "$BASH_VERSINFO" ] && [ "${BASH_VERSINFO[0]}" -lt 4 ] && echo "This library requires Bash 4 or higher" && exit 1 || true
 
-if [[ "$BOOTSTRAPPED" == true ]]; then
+if [[ "${BOOTSTRAPPED:-}" == true ]]; then
     logger.warn 'Already bootstrapped. Skipping...'
     return 0
 fi
 
-# Fail a pipe command if any of the commands fail
-set -o pipefail
-# Error if a variable is used before being set.
-set -u
-# Allow aliases for more advanced behavior.
-shopt -s expand_aliases
+# Set shell options for running in a production environment.
+# We strongly recommend you use these options.
+# Only disable if this is NOT running in production.
+# To test interactively, use a login shell (i.e. `bash -l`)
+if [[ "${UNSAFE:-}" != thisisnotproduction ]]; then
+    if ! shopt -q login_shell; then
+        # Exit if any command returns a non-zero code.
+        set -e
+    fi
+    # Fail a pipe command if any of the commands fail.
+    set -o pipefail
+    # Allow aliases for more advanced behavior.
+    shopt -s expand_aliases
+fi
 
 # Get absolute path to the repository root
-__repo_root=$(cd "${BASH_SOURCE[0]%/*}" && pwd -P)
+__repo_root=${BASH_SOURCE[0]%/*}
+if [[ "${__repo_root}" == "${BASH_SOURCE[0]}" ]]; then
+    # Presumably sourcing from the same directory
+    __repo_root=$(pwd -P)
+elif [[ ! -d "${__repo_root}" ]]; then
+    echo 'Failed to find bootstrap source directory.'
+    return 1
+else
+    __repo_root=$(cd "${BASH_SOURCE[0]%/*}" && pwd -P)
+fi
 
 # Immediately try and make our lives easier: include a simple logger and an importer.
 # shellcheck source=lib/utils/logger.sh
